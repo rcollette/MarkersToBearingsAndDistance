743d7e11f247358f9fdec1853f82517020df89eb
Rich Collette <richard.collette@precisely.com>
2024-02-17 17:46:18 -0500

    Initial commit

---
+++ src/app/home/geojson.ts
@@ -0,0 +1,523 @@
+import { FeatureCollection, Point } from "geojson";
+
+export const data: FeatureCollection<Point> = {
+  "features": [{
+    "geometry": {
+      "coordinates": [-71.77754938602449, 41.52100324919817, 0, 0], "type": "Point"
+    }, "id": "0378b6ef-a4ee-4f20-b0d9-327fa8521041", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "34 North East Go Past Buildings to Road",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877997439,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77401423454286, 41.51588011766112, 0, 0], "type": "Point"
+    }, "id": "0821ebcb-bc14-407c-811b-7ef598342eec", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "30 North to Road",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877796216,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78490400314332, 41.51277913072961, 0, 0], "type": "Point"
+    }, "id": "0a18c590-0d41-4a7f-b2e6-70eea8791f8d", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "23 Fork Offroad SouthEast",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877561974,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78838014602663, 41.51534187410812, 0, 0], "type": "Point"
+    }, "id": "1fea84ec-7043-4fcb-8986-8b8ba56b41d4", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "20 SSW Through marsh",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877234974,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78211450576784, 41.511831130982316, 0, 0], "type": "Point"
+    }, "id": "2370288b-a610-4ead-bb35-367558ca755f", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "24 East-NorthEast at high ground",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877474186,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.7836916446686, 41.530314621804735, 0, 0], "type": "Point"
+    }, "id": "23a7f61e-97e1-4b98-9493-e83981af81cc", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "05 Hidden Lake Trail on Left",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875486044,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78912043571474, 41.52579263883068, 0, 0], "type": "Point"
+    }, "id": "299cdb24-b24c-4caa-a2c6-3d5ee6ea3cf6", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "14 Cross Yawgoog Rd. to Round the Pond Trail",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707876950804,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.7921566963196, 41.526852883451994, 0, 0], "type": "Point"
+    }, "id": "29fe833f-45ae-4bb6-a3eb-4c978cd8e212", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "11 Yawgoog Road Turn East",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875853660,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78894340991975, 41.52678059459791, 0, 0], "type": "Point"
+    }, "id": "2bbd6e5a-f268-462e-888e-a6665b4e4d10", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "13 Hidden Lake - Trail Fork -  Turn South",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875911033,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.7749583721161, 41.51918981545095, 0, 0], "type": "Point"
+    }, "id": "2e8af272-a3af-47e5-a0b0-a911edeb3bd8", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "32 West Onto Bucklin Walkway",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877894629,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78898096084596, 41.53519367164296, 0, 0], "type": "Point"
+    }, "id": "2e9b50eb-9c9a-47ec-a18b-684bc354f312", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "07 Turn towards West",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875678160,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78584277629854, 41.51278314764814, 0, 0], "type": "Point"
+    }, "id": "33c97547-9c33-414f-8621-12dc0ef444f4", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "22 East on Road",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707879244198,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77822530269624, 41.52490908837444, 0, 0], "type": "Point"
+    }, "id": "44ee2cdf-12f6-4476-8bac-3a59c62999cc", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "01 Start -  Archery Range",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1708119919416,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.7908799648285, 41.5178000843225, 0, 0], "type": "Point"
+    }, "id": "48df5595-e05e-4186-9f3e-a85faec27f7b", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "18 Coves End Short Jog East",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877172267,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.79148077964784, 41.52082853520207, 0, 0], "type": "Point"
+    }, "id": "4d6ccd87-9955-4740-972e-da1c07a20e6b", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "16 Back to and follow stream",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877056166,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77973270416261, 41.51732612295172, 0, 0], "type": "Point"
+    }, "id": "552925e9-ccfd-4da2-a5c0-70843ae2d945", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "27 Southeast Through Campsites",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877659905,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.79163098335268, 41.534952739548125, 0, 0], "type": "Point"
+    }, "id": "5ae0578f-ebc0-4d01-8279-75b639e9fe7a", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "09 Turn Southwest",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875723504,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77952885627748, 41.513060314425665, 0, 0], "type": "Point"
+    }, "id": "5d19be85-8417-4209-b82d-e27876e6c8e7", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "25 North-Northwest",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877505834,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78834795951845, 41.53220605023532, 0, 0], "type": "Point"
+    }, "id": "5f877e95-c957-4af3-9639-80843c3001d2", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "06 Turn Towards North",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875543585,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.789887547493, 41.524093801568874, 0, 0], "type": "Point"
+    }, "id": "63c05e99-0234-447a-8169-1f4217b96463", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "15 Stream Crossing",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877039341,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77711486816408, 41.51427340722673, 0, 0], "type": "Point"
+    }, "id": "6aca8cb9-a0fc-496b-88b4-98cec8a985b1", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "28 Northeast at Peninsula",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877705096,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.7871677875519, 41.51348610455453, 0, 0], "type": "Point"
+    }, "id": "7d11e84e-60a6-4cf6-8dd9-5ab12d21b6cb", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "21 Fork to Southeast",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707879184396,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.79027915000917, 41.51772778535471, 0, 0], "type": "Point"
+    }, "id": "8064fc34-c585-4b4b-985e-0d2ac5788f8c", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "19 Turn South",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877363370,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77607953548433, 41.51905124533394, 0, 0], "type": "Point"
+    }, "id": "89d17097-c1e4-4e1c-bda6-addb9edfccc2", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "33 Northwest on Marvel Rd.",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877948860,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77815556526186, 41.52484081351883, 0, 0], "type": "Point"
+    }, "id": "984ec8ae-3c5f-4590-a6c3-b9b03d2bef23", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "37 Finish - Archery Range",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1708119998290,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.7766508460045, 41.52295117789692, 0, 0], "type": "Point"
+    }, "id": "9e7532b1-43c4-425a-a22f-2e42dbc29d76", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "36 Rifle Range on Right",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1708119955228,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77463650703432, 41.51806517984683, 0, 0], "type": "Point"
+    }, "id": "a2d60b0a-0de6-4df2-8c8c-63be4570b818", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "31 Offroad on Rt Trail to Bucklin Parking",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877852417,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78007870912553, 41.52590508984141, 0, 0], "type": "Point"
+    }, "id": "a59c5d5c-7d43-4370-a9cd-ad6538a1b270", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "03 Start General Dean Trail",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875360441,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78910434246065, 41.525788622719524, 0, 0], "type": "Point"
+    }, "id": "a79d7b4d-cb3b-48d3-a433-8d05a7d6d334", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "12 Turn North to Hidden Lake",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875852251,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78128838539125, 41.51614522104948, 0, 0], "type": "Point"
+    }, "id": "af96182f-c37f-44b4-989e-87c200797a0a", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "26 Northeast on Road",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877602292,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.78121328353883, 41.5287685086555, 0, 0], "type": "Point"
+    }, "id": "c3f05d2e-68e8-4e47-91d5-9d910c3c3627", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "04 Left of Fork in Road",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875369626,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77606344223024, 41.51588011766112, 0, 0], "type": "Point"
+    }, "id": "cd746b57-6f84-4427-ac78-765817fd8b09", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "29 East",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877762308,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.79179191589357, 41.519543268318834, 0, 0], "type": "Point"
+    }, "id": "d2ce7879-d1ad-4272-90b4-25bc750a2c0b", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "17 SSW to Coves End",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707877126175,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.79014503955842, 41.53543460284035, 0, 0], "type": "Point"
+    }, "id": "d5372083-8d8e-4f39-a28d-0f59a0e69265", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "08 Turn WSW",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875698003,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.77894413471223, 41.525752477708025, 0, 0], "type": "Point"
+    }, "id": "d53bd3f5-6632-47f4-8100-dfdba01ab81e", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "02 Left Turn in Road",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875347129,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.7758032679558, 41.521617756747716, 0, 0], "type": "Point"
+    }, "id": "e2adf0b4-eb39-45e1-b9d2-2b3ef2291dee", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "35 NorthWest back to Archery Range on Road",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707878031500,
+      "marker-rotation": null
+    }
+  }, {
+    "geometry": {
+      "coordinates": [-71.79193675518037, 41.53432631190147, 0, 0], "type": "Point"
+    }, "id": "f0be70ae-a0ff-4136-83a5-5f6f7446da56", "type": "Feature", "properties": {
+      "marker-symbol": "point",
+      "creator": "KL8VA0",
+      "marker-color": "FF0000",
+      "description": "",
+      "title": "10 Turn South",
+      "marker-size": "1",
+      "class": "Marker",
+      "updated": 1707875778206,
+      "marker-rotation": null
+    }
+  }], "type": "FeatureCollection"
+};
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,112 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from "rxjs";
+import { TableModule } from "primeng/table";
+import { AsyncPipe, DecimalPipe, JsonPipe } from "@angular/common";
+import { Feature, FeatureCollection, GeoJsonProperties, Point } from "geojson";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputTextModule } from "primeng/inputtext";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import { ButtonModule } from "primeng/button";
+
+const semiPi = Math.PI / 180;
+
+interface IBearingAndDistance {
+  originFeature: Feature<Point>;
+  targetFeature: Feature<Point>;
+  trueBearingDegrees: number;
+  magneticBearingDegrees: number;
+  distanceFeet: number;
+}
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [TableModule, AsyncPipe, JsonPipe, DecimalPipe, InputGroupModule, InputTextModule, ButtonModule, ReactiveFormsModule,InputTextareaModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+  public geoJsonDataControl = new FormControl<string>('');
+  public declinationControl = new FormControl<number>(0);
+  public formGroup = new FormGroup({ 'geoJsonData': this.geoJsonDataControl, 'declination': this.declinationControl });
+
+  public bearingAndDistance: BehaviorSubject<IBearingAndDistance[]> = new BehaviorSubject<IBearingAndDistance[]>([]);
+
+  ngOnInit() {
+    // data.features = data.features.sort((a, b) => a.properties?.['title'] < b.properties?.['title']?-1:1);
+    // this.pointsToBearingsAndDistance(data, -13.7);
+  }
+
+  public calculate() {
+    if (!this.geoJsonDataControl.value || !this.declinationControl.value) {
+      return;
+    }
+    const geoJsonData: FeatureCollection<Point> = JSON.parse(this.geoJsonDataControl.value);
+    if (!geoJsonData || !geoJsonData.features || geoJsonData.features.length < 2) {
+      return;
+    }
+    geoJsonData.features = geoJsonData.features.sort((a, b) => a.properties?.['title'] < b.properties?.['title'] ? -1 : 1);
+    this.pointsToBearingsAndDistance(geoJsonData, this.declinationControl.value);
+  }
+
+  pointsToBearingsAndDistance(geoJsonData: FeatureCollection<Point>, declination: number) {
+    if (!geoJsonData || !geoJsonData.features || geoJsonData.features.length < 2) {
+      throw new Error('Invalid GeoJsonData.  Must have at least 2 features.');
+    }
+    const results: IBearingAndDistance[] = [];
+    for (let i = 1; i < geoJsonData.features.length; i++) {
+      results.push(this.calculateDistanceAndBearing(geoJsonData.features[i - 1], geoJsonData.features[i], declination));
+    }
+    const totalDistance = results.reduce((acc, cur) => acc + cur.distanceFeet, 0);
+    console.log('Total Distance Miles: ', totalDistance / 5280);
+    this.bearingAndDistance.next(results);
+  }
+
+  private calculateDistanceAndBearing(originFeature: Feature<Point, GeoJsonProperties>, targetFeature: Feature<Point, GeoJsonProperties>, declination: number): IBearingAndDistance {
+    const originPosition = originFeature.geometry.coordinates;
+    const targetPosition = targetFeature.geometry.coordinates;
+    // Convert latitude and longitude from degrees to radians
+    const lat1 = this.toRadians(originPosition[1]);
+    const lon1 = this.toRadians(originPosition[0]);
+    const lat2 = this.toRadians(targetPosition[1]);
+    const lon2 = this.toRadians(targetPosition[0]);
+
+    // Haversine formula to calculate the distance
+    const distanceMeters = this.haversineDistance(lon2, lon1, lat2, lat1);
+
+    // Convert distance from meters to feet
+    const distanceFeet = distanceMeters * 3.28084;
+
+    // Formula to calculate the True Bearing
+    let trueBearingDegrees = this.trueBearing(lat2, lon2, lon1, lat1);
+
+    // Adjust True Bearing to Magnetic Bearing
+    const magneticBearingDegrees = trueBearingDegrees + declination;
+
+    return {
+      originFeature, targetFeature, trueBearingDegrees, magneticBearingDegrees, distanceFeet
+    }
+  }
+
+  private trueBearing(lat2: number, lon2: number, lon1: number, lat1: number) {
+    const x = Math.cos(lat2) * Math.sin(lon2 - lon1);
+    const y = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1);
+    let trueBearingDegrees = Math.atan2(x, y) * 180 / Math.PI;
+    return (trueBearingDegrees + 360) % 360;
+  }
+
+  private haversineDistance(lon2: number, lon1: number, lat2: number, lat1: number) {
+    const dLon = lon2 - lon1;
+    const dLat = lat2 - lat1;
+    const a = Math.sin(dLat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) ** 2;
+    const c = 2 * Math.asin(Math.sqrt(a));
+    const r = 6371e3; // Radius of the Earth in meters
+    return c * r;
+  }
+
+  private toRadians(degrees: number): number {
+    return degrees * semiPi;
+  }
+
+}
